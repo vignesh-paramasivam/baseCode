81ff364702cb4a10393eec737b4856f53c849ae9
vignesh-paramasivam <vignesh.paramasivam@moolya.com>
2020-03-19 12:57:14 +0530

    jsonBody dynamic method - to work on json ary inside json ary - code modification done.

--- src/test/java/testcore/api/Builders/CitizenComplaintCreationBuilder.java
+++ src/test/java/testcore/api/Builders/CitizenComplaintCreationBuilder.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.codehaus.jettison.json.JSONTokener;
+import org.testng.Assert;
 
 
 public class CitizenComplaintCreationBuilder {
@@ -52,7 +53,17 @@ public class CitizenComplaintCreationBuilder {
 
     private JSONArray actionInfo() throws Exception {
         actionInfo = new JSONArray();
-        actionInfo.put(new JSONObject().put("media", new JSONArray()));
+        actionInfo.put(new JSONObject()
+                .put("media", new JSONArray()))
+                //Added for testing
+                .put(new JSONObject()
+                        .put("role", new JSONArray()
+                                .put(new JSONArray().put(new JSONObject()
+                                        .put("a", 1)
+                                        .put("b", 2)))
+                                .put(new JSONArray().put(new JSONObject()
+                                        .put("c", 3)
+                                        .put("d", 4)))));
 
         return actionInfo;
     }
@@ -63,21 +74,14 @@ public class CitizenComplaintCreationBuilder {
         services.put(new JSONObject()
                 .put("serviceCode", "GarbageNeedsTobeCleared")
                 .put("description", "test")
-                .put("addressDetail", new JSONArray().put(new JSONObject()
-                        .put("latitude", "31.322422")
-                        .put("longitude", "75.573419")
-                        .put("city", "pb.jalandhar")
-                        .put("mohalla", "JLC216")
-                        .put("houseNoAndStreetName", "test")
-                        .put("landmark", "test"))
-                        .put(new JSONObject()
-                                        .put("latitude", "21.322422")
-                                        .put("longitude", "21.573419")
-                                        .put("city", "bangalore")
-                                        .put("mohalla", "JLC216")
-                                        .put("houseNoAndStreetName", "123")
-                                        .put("landmark", "123"))
-                        )
+                .put("addressDetail",new JSONObject()
+                                .put("latitude", "21.322422")
+                                .put("longitude", "21.573419")
+                                .put("city", "bangalore")
+                                .put("mohalla", "JLC216")
+                                .put("houseNoAndStreetName", "123")
+                                .put("landmark", "123")
+                )
                 .put("address", "Jalandhar,Nakodar Rd, Shaheed Udham Singh Nagar, Jalandhar, Punjab 144001, India")
                 .put("tenantId", "pb.jalandhar")
                 .put("source", "web")
@@ -110,53 +114,75 @@ public class CitizenComplaintCreationBuilder {
         return this;
     }
 
-    
+
     //If JSONArray value is getting added the Key can be null
     public CitizenComplaintCreationBuilder add(String key_parent, String key, Object value) throws Exception {
         String[] keys = key_parent.split("\\|");
 
         Object jObj = jsonObject;
 
-        JSONObject jObjInstance;
-        JSONArray jArrInstance;
-
         for(String _key: keys) {
             // Check if object is JSON array
             String[] keyCheck = _key.split("\\{");
             //TODO 1: Accessing json arrays inside json array to be done
-            if(((JSONObject) jObj).optJSONArray(keyCheck[0]) != null) {
-
+            if(isJsonArray(jObj)){
+                if(keyCheck.length > 1) {
+                    jObj = getKeyInDepth(_key, jObj);
+                } else {
+                    /*Todo: identify objects in depth instead of first object*/
+                   jObj = ((JSONObject)((JSONArray) jObj).get(0)).getJSONArray(_key);
+                }
+            }
+            else if(((JSONObject) jObj).optJSONArray(keyCheck[0]) != null) {
                 //if we need to get the object inside json array
                 if(keyCheck.length > 1) {
                     jObj = getKeyInDepth(_key, jObj);
                 } else {
-                    jObj = ((JSONObject) jObj).getJSONArray(_key).get(0);
-                }
+                    jObj = ((JSONObject) jObj).getJSONArray(_key); }
             } else if (((JSONObject) jObj).optJSONObject(_key) != null) {
-                jObjInstance = (JSONObject) jObj;
-                jObj = jObjInstance.getJSONObject(_key);
+                jObj = ((JSONObject) jObj).getJSONObject(_key); }
+            else {
+                Assert.fail(_key + "object is not available in the json");
             }
         }
 
-        Object jSn = new JSONTokener(jObj.toString()).nextValue();
+        addItemToJson(jObj, key, value);
 
-        if(jSn instanceof JSONArray) {
+        return this;
+    }
+
+    private void addItemToJson(Object jObj, String key, Object value) throws Exception {
+        JSONObject jObjInstance;
+        JSONArray jArrInstance;
+
+        if(isJsonArray(jObj)) {
             //TODO 2: May get class cast exception - need to address
             jArrInstance = (JSONArray) jObj;
             jArrInstance.put(value);
-
-        } else if (jSn instanceof JSONObject){
+        } else {
             jObjInstance = (JSONObject) jObj;
             jObjInstance.put(key, value);
         }
-        return this;
+    }
+
+    private boolean isJsonArray(Object obj) throws Exception {
+        Object thisObj = new JSONTokener(obj.toString()).nextValue();
+        return thisObj instanceof JSONArray;
     }
 
     private Object getKeyInDepth(String _key, Object Obj) throws Exception {
         String key = _key.split("\\{")[0];
         String[] valuesToCheck = StringUtils.substringBetween(_key, "{", "}").split(";");
-        JSONArray tempArr = ((JSONObject) Obj).getJSONArray(key);
-        int availableObjs = ((JSONObject) Obj).getJSONArray(key).length();
+        JSONArray tempArr;
+        int availableObjs;
+
+        if(isJsonArray(Obj)) {
+            tempArr = (JSONArray) Obj;
+            availableObjs = ((JSONArray) Obj).length();
+        } else {
+            tempArr = ((JSONObject) Obj).getJSONArray(key);
+            availableObjs = ((JSONObject) Obj).getJSONArray(key).length();
+        }
 
         boolean objectPresent = true;
         Object tempObj = null;
@@ -164,12 +190,17 @@ public class CitizenComplaintCreationBuilder {
         for(int i = 0; i < availableObjs; i++) {
             tempObj = tempArr.get(i);
 
+            //Todo: Inprogress: Processing only JSONObjects - need to add capability to idenitify JSONArray
+            if(isJsonArray(tempObj)) {
+                tempObj = ((JSONObject) tempObj).get(key);
+            } else {
+                tempObj = ((JSONObject) tempObj).get(key);
+            }
+
             for (String val: valuesToCheck) {
-              String[] keyVal = val.split(":");
-              //TODO 3: All values are converted to string and compared. This needs to be validated based on data types
-                if(!((JSONObject) tempObj).opt(keyVal[0]).toString().equals(keyVal[1])){
-                    objectPresent = false;
-                };
+                String[] keyVal = val.split(":");
+                //TODO 3: All values are converted to string and compared. This needs to be validated based on data types
+                if(!((JSONObject) tempObj).opt(keyVal[0]).toString().equals(keyVal[1])){ objectPresent = false; };
             }
             if(objectPresent) { return tempObj; }
         }
