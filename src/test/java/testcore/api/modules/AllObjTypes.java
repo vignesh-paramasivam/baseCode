ee6099eb6d7b9ca1e10cc177c421b8b9a4dabeb1
vignesh-paramasivam <vignesh.paramasivam@moolya.com>
2020-03-22 22:55:17 +0530

    json input with different combinations added

--- src/test/java/testcore/api/modules/AllObjTypes.java
+++ src/test/java/testcore/api/modules/AllObjTypes.java
@@ -3,6 +3,7 @@ package testcore.api.modules;
 import agent.IAgent;
 import central.Configuration;
 import io.restassured.response.Response;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.testng.Assert;
 import testcore.api.BaseApi;
@@ -36,7 +37,7 @@ public class AllObjTypes extends BaseApi {
         builder.add("lastName", "Smith");
         builder.add("age", 25);
 
-        //Output of json up to this level - created using the builder pattern,z
+        //Output of json up to this level - created using the builder pattern,
         /*
         * {
             "firstName": "John",
@@ -57,13 +58,75 @@ public class AllObjTypes extends BaseApi {
                 *
         * */
 
+        builder.add("business", new JSONArray()
+                .put(new JSONObject()
+                        .put("type", "vehicle")
+                        .put("car", new JSONObject()
+                                .put("brand", "Toyota")
+                                .put("model", new JSONArray().put("Prius").put("Camray"))
+                                .put("quantity", 2))
+                )
+                .put(new JSONObject()
+                        .put("type", "vehicle")
+                        .put("bike", new JSONObject()
+                                .put("brand", "Yamaha")
+                                .put("model", new JSONArray().put(new JSONObject().put("new", "FZ").put("old", "FZS")))
+                                .put("quantity", 5))
+                ));
 
-        builder.add("address{postalCode:10021}", "City", "New York");
+
+        builder.add("buyers", new JSONArray()
+                .put(new JSONObject().put("online", new JSONArray())
+                        .put("offline", new JSONArray()
+                                .put("cash")
+                                .put("card"))
+                ));
+
+        //Output of json up at this level
+        /*
+                    * {
+                "firstName": "John",
+                "address": {
+                    "state": "NY",
+                    "postalCode": 12121
+                },
+                "phoneNumbers": [{
+                    "type": "home",
+                    "number": "123-111"
+                }, {
+                    "type": "fax",
+                    "number": "123-222"
+                }],
+                "lastName": "Smith",
+                "age": 25,
+                "business": [{
+                    "type": "vehicle",
+                    "car": {
+                        "brand": "Toyota",
+                        "model": ["Prius", "Camray"],
+                        "quantity": 2
+                    }
+                }, {
+                    "type": "vehicle",
+                    "bike": {
+                        "brand": "Yamaha",
+                        "model": [{
+                            "new": "FZ",
+                            "old": "FZS"
+                        }],
+                        "quantity": 5
+                    }
+                }],
+                "buyers": [{
+                    "online": [],
+                    "offline": ["cash", "card"]
+                }]
+            }*/
+
+        builder.add("address[{postalCode:10021}]", "City", "New York");
 
         return thisClass();
     }
-
-
 
 
 }
