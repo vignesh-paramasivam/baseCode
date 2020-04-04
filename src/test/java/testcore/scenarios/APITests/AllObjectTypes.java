5c451b883d983f39e501d3415a3ad8db6bd837d7
Vignesh Paramasivam <>
2020-04-04 16:47:18 +0530

    Trivial checkin

--- src/test/java/testcore/scenarios/APITests/AllObjectTypes.java
+++ src/test/java/testcore/scenarios/APITests/AllObjectTypes.java
@@ -7,7 +7,6 @@ public class AllObjectTypes extends SupportTest {
 
     @Test(enabled = true, description = "Check if we can create or update objects at any depth")
     public void validateAllObjTypes() throws Exception {
-        allObjTypes()
-                .testAddDiffTypes();
+        allObjTypes().testAddDiffTypes();
     }
 }
